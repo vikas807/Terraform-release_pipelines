e825e0553392c1aadcb1c58863e6a9e1d30379ce
Vikas Pal <vpal807@gmail.com>
2023-08-12 04:28:59 +0530

    feat: name fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={logo} alt="logo" />
-        <h1>Hello world</h1>
+        <h1>Hello World</h1>
         
       </header>
     </div>
